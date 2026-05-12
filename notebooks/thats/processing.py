8c3b58bf5e1d8e623052f022f55c2e49f4e95711
Luca Bataillard <luca.bataillard@gmail.com>
2026-05-12 15:28:30 +0100

    compare predicted and manual modes

--- notebooks/thats/processing.py
+++ notebooks/thats/processing.py
@@ -33,7 +33,7 @@ def _():
     from activitygraphs.data.overture import Overture
     from activitygraphs.dataprocessing import build_toronto_network_graph
 
-    return Overture, TorontoData
+    return Overture, TorontoData, build_toronto_network_graph
 
 
 @app.cell
@@ -45,6 +45,37 @@ def _(TorontoData):
 @app.cell
 def _(Overture, data):
     overture = Overture.load(data.locations_gdf, cfg.data.inputs.overture)
+    return (overture,)
+
+
+@app.cell
+def _(build_toronto_network_graph, data, overture):
+    network_nodes, network_edges = build_toronto_network_graph(
+        data.locations_gdf, overture, None
+    )
+    return network_edges, network_nodes
+
+
+@app.cell
+def _(network_nodes):
+    select_node_col = mo.ui.dropdown(
+        list(network_nodes.columns), searchable=True, label="Column:"
+    )
+    return (select_node_col,)
+
+
+@app.cell
+def _(network_edges, network_nodes, select_node_col):
+    _nodes = network_nodes  # .set_geometry("original_geometry") #if toggle_polygons.value else indiv_nodes
+    _m = network_edges.explore(color="gray", tiles="Cartodb Positron")
+    _m = _nodes.explore(
+        m=_m, column=select_node_col.value, marker_kwds={"radius": 5}
+    )
+
+    mo.vstack([
+        select_node_col,  # mo.hstack([select_user_id, select_node_col, toggle_polygons], justify="start"),
+        _m,
+    ])
     return
 
 
@@ -114,9 +145,15 @@ def _():
 
 @app.cell
 def _(alt, data, pl):
-    _mode_label_freq = data.inputs.raw_journeys_df.group_by("app_id").agg(pct_labeled=pl.col("manual_mode").is_not_null().sum() / pl.len())
+    _mode_label_freq = data.inputs.raw_journeys_df.group_by("app_id").agg(
+        pct_labeled=pl.col("manual_mode").is_not_null().sum() / pl.len()
+    )
 
-    _chart = _mode_label_freq.plot.bar(x=alt.X("pct_labeled", bin=alt.Bin(maxbins=20)), y="count()").properties(title="Distribution of proportion of manually-labeled trip modes per user")
+    _chart = _mode_label_freq.plot.bar(
+        x=alt.X("pct_labeled", bin=alt.Bin(maxbins=20)), y="count()"
+    ).properties(
+        title="Distribution of proportion of manually-labeled trip modes per user"
+    )
     _chart
     return
 
@@ -126,6 +163,8 @@ def _(alt, itertools, pl):
     def plot_enum_confusion_matrix(
         df: pl.DataFrame, pred_col: str, actual_col: str, enum, scale_type="linear"
     ) -> alt.Chart:
+        width, height = 400, 400
+    
         base = pl.DataFrame(
             list(itertools.product(enum, enum)),
             orient="row",
@@ -136,7 +175,14 @@ def _(alt, itertools, pl):
         modes = base.join(modes, on=[pred_col, actual_col], how="left").fill_null(
             0
         )
-        modes = modes.sort("predicted_modes", "manual_mode")
+        modes = modes.sort(pred_col, actual_col)
+
+        row_totals = modes.group_by(actual_col, maintain_order=True).agg(
+            pl.col("count").sum()
+        )
+        col_totals = modes.group_by(pred_col, maintain_order=True).agg(
+            pl.col("count").sum()
+        )
 
         labels = (
             alt
@@ -145,9 +191,7 @@ def _(alt, itertools, pl):
             .encode(
                 x=f"{pred_col}:N",
                 y=f"{actual_col}:N",
-                text=alt.Text(
-                    "count:Q",
-                ),
+                text=alt.Text("count:Q"),
             )
         )
 
@@ -164,10 +208,58 @@ def _(alt, itertools, pl):
                 tooltip=[pred_col, actual_col, "count"],
             )
         )
+    
+        row_boxes = (
+            alt
+            .Chart(row_totals)
+            .mark_rect()
+            .encode(
+                y=alt.Y(f"{actual_col}:N", axis=None),
+                color=alt.Color(
+                    "count:Q", scale=alt.Scale(type=scale_type, domainMin=1),
+                ),
+            )
+        )
 
-        return (boxes + labels).properties(
-            width=400,
-            height=400,
+        row_labels = (
+            alt
+            .Chart(row_totals)
+            .mark_text()
+            .encode(
+                y=alt.Y(f"{actual_col}:N", ),
+                text=alt.Text("count:Q"),
+            )
+        )
+
+        col_boxes = (
+            alt
+            .Chart(col_totals)
+            .mark_rect()
+            .encode(
+                x=alt.X(f"{pred_col}:N", axis=None),
+                color=alt.Color(
+                    "count:Q", scale=alt.Scale(type=scale_type, domainMin=1),
+                ),
+            )
+        )
+
+        col_labels = (
+            alt
+            .Chart(col_totals)
+            .mark_text()
+            .encode(
+                x=alt.X(f"{pred_col}:N", ),
+                text=alt.Text("count:Q"),
+            )
+        )
+
+        main = (boxes + labels).properties(width=width, height=height)
+        rows = (row_boxes + row_labels).properties(width=30, height=height)
+        cols = (col_boxes + col_labels).properties(width=width, height=30)
+
+        bottom = (main | rows).resolve_scale(y='shared')
+
+        return (cols & bottom).resolve_scale(x='shared', color='shared').properties(
             title="Confusion matrix of GPS predicted vs manually-entered modes",
         )
 
@@ -224,9 +316,54 @@ def _(
 def _():
     # Purposes (1-2 hrs max)
 
-    # TODO Link purposes from activities to trips, then compare with accuracy from manually labeled trip purposes
-    # TODO What proportion of unlabled trip purposes actually have corresponding data
-    # TODO Rule finding ML algorithm (perhaps)
+
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## Purposes
+
+    - Link purposes from activities to trips, then compare with accuracy from manually labeled trip purposes
+    """)
+    return
+
+
+@app.cell
+def _(data):
+    dir(data)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    - What proportion of unlabled trip purposes actually have corresponding data
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    - Rule finding ML algorithm (perhaps)
+    """)
+    return
+
+
+@app.cell
+def _():
     return
 
 
